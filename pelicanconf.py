1dbb08f1d19e793cd1af041743b3e228fee89326
Josh McVey <yersh13@gmail.com>
2019-04-10 09:51:06 -0500

    pelican

---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = "Josh"
+SITENAME = "Josh McVey"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = "America/Chicago"
+
+DEFAULT_LANG = "en"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "http://getpelican.com/"),
+    ("Python.org", "http://python.org/"),
+    ("Jinja2", "http://jinja.pocoo.org/"),
+)
+
+# Social widget
+SOCIAL = (
+    ("Github", "https://github.com/y3rsh"),
+    ("Twitter", "https://twitter.com/y3rsh"),
+    ("Instagram", "https://www.instagram.com/y3rsh/"),
+)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
